5cc1f3aad78fadaf060abbaeffffe19023552cce
Jaton Justice <masterjx9@gmail.com>
2023-09-02 11:51:36 -0700

    added handler for ig

--- handler.py
+++ handler.py
@@ -1,19 +1,26 @@
-# handler.py
-import dotenv
 import os
+import logging
 
-dotenv.load_dotenv()
+# Setup logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 from main import dailyPostToIG, dailyPostToTwitter
 
 IG_ACCOUNT1_ID = os.environ.get('IG_ACCOUNT1_ID')
 IG_ACCOUNT2_ID = os.environ.get('IG_ACCOUNT2_ID')
+
 def lambda_handler(event, context):
     scenario = event.get('scenario', 'default')
     
-    if scenario == 'instagram':
-        dailyPostToIG(IG_ACCOUNT2_ID)
-    elif scenario == 'twitter':
-        dailyPostToTwitter()
-    else:
-        print("No valid scenario provided.")
+    try:
+        if scenario == 'instagram':
+            logger.info("Executing Instagram scenario.")
+            dailyPostToIG(IG_ACCOUNT2_ID)
+        elif scenario == 'twitter':
+            logger.info("Executing Twitter scenario.")
+            dailyPostToTwitter()
+        else:
+            logger.warning("No valid scenario provided.")
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
