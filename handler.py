81bd1379034557724440ded46114daf5498a5a11
Jaton Justice <masterjx9@gmail.com>
2023-09-04 21:13:32 -0700

    added pip installs to zip

--- handler.py
+++ handler.py
@@ -12,16 +12,16 @@ IG_ACCOUNT2_ID = os.environ.get('IG_ACCOUNT2_ID')
 
 def lambda_handler(event, context):
     print("fucking work")
-    # scenario = event.get('scenario', 'default')
+    scenario = event.get('scenario', 'default')
     
-    # try:
-    #     if scenario == 'instagram':
-    #         logger.info("Executing Instagram scenario.")
-    #         dailyPostToIG(IG_ACCOUNT2_ID)
-    #     elif scenario == 'twitter':
-    #         logger.info("Executing Twitter scenario.")
-    #         dailyPostToTwitter()
-    #     else:
-    #         logger.warning("No valid scenario provided.")
-    # except Exception as e:
-    #     logger.error(f"An error occurred: {e}")
+    try:
+        if scenario == 'instagram':
+            logger.info("Executing Instagram scenario.")
+            dailyPostToIG(IG_ACCOUNT2_ID)
+        elif scenario == 'twitter':
+            logger.info("Executing Twitter scenario.")
+            dailyPostToTwitter()
+        else:
+            logger.warning("No valid scenario provided.")
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
